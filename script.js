db121901b84d0d451c432526fcf8bb8f42c50c2f
BatHelix <bathelix@gmail.com>
2019-03-22 20:45:28 -0400

    initial for scratchwork

---
+++ script.js
@@ -0,0 +1,25 @@
+// var jack = 10;
+
+// while (jack < 60) {
+//     console.log(jack);
+//     jack++;
+// };
+
+
+// ****************************
+
+// var string = 'hello';
+// var count = 0;
+
+// while (count < string.length) {
+//     console.log(string[count]);
+//     count++;
+// }
+
+// *******************************
+
+var jackFriends = ['Josh','Chris','Dustin'];
+
+for (var i = 0; i<jackFriends.length; i++) {
+    alert (jackFriends[i]);
+}
